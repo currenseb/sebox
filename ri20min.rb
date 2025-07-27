ef3c4065459f281634da41acc8ab31668398a535
CurrenSebastian <currensebastian@gmail.com>
2025-07-27 14:27:26 -0700

    \fixed formatting issues and readded rubocop

--- ri20min.rb
+++ ri20min.rb
@@ -1,54 +1,54 @@
 class MegaGreeter
   attr_accessor :names
 
-  #Create the Object
+  # Create the Object
   def initialize(names = "World")
     @names = names
   end
 
-  #Say hi to everybody
+  # Say hi to everybody
   def say_hi
     if @names.nil?
       puts "..."
     elsif @names.respond_to?("each")
-      #iterate through list of names
+      # iterate through list of names
       @names.each do |name|
         puts "Hello #{name}!"
       end
     else
       puts "Hello #{@names}!"
-      end
+    end
     end
 
-    #Say bye to everybody
+  # Say bye to everybody
   def say_bye
     if @names.nil?
       puts "..."
     elsif @names.respond_to?("join")
-      #Join the list elements with commas
+      # Join the list elements with commas
       puts "Goodbye #{@names.join(", ")}. Come back soon!"
     else
       puts "Goodbye #{@names}. Come back soon!"
-      end
     end
-  end
+    end
+end
 
 if __FILE__ == $0
   mg = MegaGreeter.new
   mg.say_hi
   mg.say_bye
 
-  #Change name to Zeke
+  # Change name to Zeke
   mg.names = "Zeke"
   mg.say_hi
   mg.say_bye
 
-  #Change to an array of names
-  mg.names = ["Albert", "Brenda", "Charles"]
+  # Change to an array of names
+  mg.names = [ "Albert", "Brenda", "Charles" ]
   mg.say_hi
   mg.say_bye
 
-  #Change to nil
+  # Change to nil
   mg.names = nil
   mg.say_hi
   mg.say_bye
