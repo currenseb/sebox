fe785f186ca7b14b3d61b75871cf6c6e662982c7
CurrenSebastian <currensebastian@gmail.com>
2025-08-01 16:24:12 -0700

    added login and signup and edgecase of user already exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  # Fixes logic error where user signs up with already existing account
+  validates :email_address, presence: true, uniqueness: true
 end
