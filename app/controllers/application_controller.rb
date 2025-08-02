18346940773880562348cc1f582755c5ea2f3e63
CurrenSebastian <currensebastian@gmail.com>
2025-08-02 13:29:40 -0700

    added basic cart functionality and made pages look prettier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    unless current_user
+      redirect_to new_session_path, alert: "You must be logged in to access your cart."
+    end
+  end
+
+
 end
