aa50d23e042ce1ffe46044d54f9d053f503ea1fa
CurrenSebastian <currensebastian@gmail.com>
2025-08-05 21:16:50 -0700

    fixed out of stock edge case

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,17 +2,22 @@ class CartItemsController < ApplicationController
   before_action :require_login
 
   def create
-    cart = current_user.cart
     product = Product.find(params[:product_id])
 
-    item = current_cart.cart_items.find_or_initialize_by(product_id: params[:product_id])
+    if product.inventory_count.to_i <= 0
+      redirect_to product_path(product), alert: "Sorry, this product is out of stock."
+      return
+    end
+
+    cart = current_user.cart
+    item = cart.cart_items.find_or_initialize_by(product_id: product.id)
     item.quantity ||= 0
     item.quantity += 1
     item.save!
-
-    product.decrement!(:inventory_count, 1)
-    redirect_to product_path(params[:product_id]), notice: "Added to cart!"
+    redirect_to product_path(product), notice: "Added to cart!"
   end
+
+
 
   def destroy
     item = current_cart.cart_items.find(params[:id])
